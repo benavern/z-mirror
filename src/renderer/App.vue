1b39f12069a477fd4a5aea8fee9fa262f5d07d58
Benjamin Caradeuc <bencrdc@gmail.com>
2017-08-06 22:51:34 +0200

    Clock

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,24 +5,35 @@
 </template>
 
 <script>
+  import moment from 'moment'
   import ZMirror from '@/components/ZMirror'
 
   export default {
     name: 'app',
     components: {
       ZMirror
+    },
+    mounted () {
+      moment.locale('fr')
     }
   }
 </script>
 
 <style lang="sass">
+
+  @import url('https://fonts.googleapis.com/css?family=Quicksand:300,400,700')
+
   *
     box-sizing: border-box
     margin: 0
     padding: 0
 
+  html
+    font-size: 20px
+
   body 
-    font-family: sans-serif
+    font-family: "Quicksand", sans-serif
     background-color: #000
+    line-height: 1.3
 
 </style>
--- src/renderer/components/Clock/Clock.vue
+++ src/renderer/components/Clock/Clock.vue
@@ -1,16 +1,51 @@
 <template>
   <div id="clock">
-    00:00
+    <div class="time">{{ time }}</div>
+    <div class="date">{{ date }}</div>
   </div>
 </template>
 
 <script>
+  import moment from 'moment'
+
   export default {
-    name: 'clock'
+    name: 'clock',
+    data () {
+      return {
+        time: '',
+        date: '',
+        clockTimer: null
+      }
+    },
+    mounted () {
+      this.$on('start', this.startClock)
+      this.$on('stop', this.stopClock)
+
+      this.updateClock()
+      this.startClock()
+    },
+    methods: {
+      updateClock () {
+        const now = moment()
+        console.log(now)
+        this.time = now.format('HH:mm')
+        this.date = now.format('dddd D MMM YYYY')
+      },
+      startClock () {
+        this.clockTimer = setInterval(this.updateClock, 1000)
+      },
+      stopClock () {
+        this.clockTimer = clearInterval(this.updateClock)
+      }
+    }
   }
 </script>
 
 <style lang="sass" scoped>
   #clock
-    text-align: center
+    .time
+      font-size: 3rem
+      font-weight: lighter
+    .date
+      color: #ccc
 </style>
